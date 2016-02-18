1a31028edcb0a2082f96633691b01a20d36650f9
wrathematics <wrathematics@gmail.com>
2016-02-18 09:17:18 -0500

    triple colon 'import' for now

--- R/state.r
+++ R/state.r
@@ -1,6 +1,6 @@
 pbd_reset_state <- function()
 {
-  reset_state()
+  invisible(eval(parse(text = "remoter:::reset_state()")))
   set(auto.dmat, TRUE)
   set(remote_port, 55556)
   set(bcast_method, "zmq")
@@ -14,30 +14,27 @@ pbd_reset_state <- function()
 
 
 
+set <- eval(parse(text = "remoter:::set"))
+getval <- eval(parse(text = "remoter:::getval"))
+get.status <- eval(parse(text = "remoter:::get.status"))
+set.status <- eval(parse(text = "remoter:::set.status"))
 
 
-set <- function(var, val)
+remoter_warning <- eval(parse(text = "remoter:::remoter_warning"))
+remoter_error <- eval(parse(text = "remoter:::remoter_error"))
+remoter_repl_server <- eval(parse(text = "remoter:::remoter_repl_server"))
+validate_port <- eval(parse(text = "remoter:::validate_port"))
+
+logprint <- eval(parse(text = "remoter:::logprint"))
+mpilogprint <- function(msg, checkverbose=FALSE, checkshowmsg=FALSE, preprint="", level="")
 {
-  name <- as.character(substitute(var))
-  .pbdenv[[name]] <- val
-  invisible()
+  if (comm.rank() == 0)
+    logprint(msg, checkverbose, checkshowmsg, preprint, level)
 }
 
-getval <- function(var)
-{
-  name <- as.character(substitute(var))
-  .pbdenv[[name]]
-}
+remoter_send <- eval(parse(text = "remoter:::remoter_send"))
+remoter_receive <- eval(parse(text = "remoter:::remoter_receive"))
 
-get.status <- function(var)
-{
-  name <- as.character(substitute(var))
-  .pbdenv$status[[name]]
-}
-
-set.status <- function(var, val)
-{
-  name <- as.character(substitute(var))
-  .pbdenv$status[[name]] <- val
-  invisible()
-}
+remoter_check_password_remote <- eval(parse(text = "remoter:::remoter_check_password_remote"))
+remoter_check_version_remote <- eval(parse(text = "remoter:::remoter_check_version_remote"))
+magicmsg_first_connection <- eval(parse(text = "remoter:::magicmsg_first_connection"))
