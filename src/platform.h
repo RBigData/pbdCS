47316fae8917f4bc5374573e4ac6c93a218a2cf8
Drew Schmidt <wrathematics@gmail.com>
2016-07-04 10:40:40 -0400

    cleaner platform checks

--- src/platform.h
+++ src/platform.h
@@ -1,4 +1,4 @@
-/*  Copyright (c) 2015, Drew Schmidt
+/*  Copyright (c) 2014-2015, Drew Schmidt
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -25,69 +25,19 @@
 */
 
 
-#include <R.h>
-#include <Rinternals.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
 
-#include "platform.h"
 
-#if !OS_WINDOWS
-#include <sys/types.h>
-#include <ifaddrs.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-
-#define LOCALHOST "127."
-
-// FIXME this SHOULD be in net/if.h, but doesn't get included for some insane reason
-#ifndef IFF_LOOPBACK
-#define IFF_LOOPBACK 0 // skip if undefined
-#endif
+// "portability"
+//#define CYGWIN_OR_MINGW (defined(__MINGW32__) || defined(__MINGW64__) || defined(__CYGWIN__) || defined(__CYGWIN32__))
+#define OS_LINUX (defined(__gnu_linux__) || defined(__linux__) || defined(__linux))
+#define OS_WINDOWS (defined(__WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(__WIN64) || defined(__WIN64__) || defined(__TOS_WIN__) || defined(__WINNT) || defined(__WINNT__))
+#define OS_MAC (defined(__APPLE__) && defined(__MACH__))
+#define OS_FREEBSD (defined(__FreeBSD__))
+#define OS_BSD (defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragonFly__))
+#define OS_SOLARIS (defined(__sun) || defined(sun))
+#define OS_HURD (defined(__GNU__) || defined(__gnu_hurd__)) // why the hell not
+#define OS_NIX (OS_BSD || OS_HURD || OS_LINUX || OS_MAC || OS_SOLARIS)
 
 #endif
-
-// hope they don't do something weird lol
-SEXP pbdcs_getip()
-{
-  SEXP ip;
-  #if !OS_WINDOWS
-  struct ifaddrs *tmp, *ifap;
-  struct sockaddr_in *pAddr;
-  char *addr;
-  
-  getifaddrs(&ifap);
-  tmp = ifap;
-  
-  while (tmp)
-  {
-    if (tmp->ifa_addr && tmp->ifa_addr->sa_family == AF_INET)
-    {
-      pAddr = (struct sockaddr_in *) tmp->ifa_addr;
-      
-      addr = inet_ntoa(pAddr->sin_addr);
-      
-      if (strcmp(tmp->ifa_name, "lo") != 0  && 
-          strcmp(addr, LOCALHOST)     != 0  && 
-          !(tmp->ifa_flags & IFF_LOOPBACK)  )
-      {
-        PROTECT(ip = allocVector(STRSXP, 1));
-        SET_STRING_ELT(ip, 0, mkChar(addr));
-        free(ifap);
-        UNPROTECT(1);
-        return ip;
-      }
-    }
-    
-    tmp = tmp->ifa_next;
-  }
-  
-  freeifaddrs(ifap);
-  #endif
-  
-  return R_NilValue;
-}
